0a156cae2488e7d1eab96750855470d4ef60c07e
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-09-29 15:50:38 -0400

    build(Task): created the entity Task

--- TaskTracker/Entities/Task.cs
+++ TaskTracker/Entities/Task.cs
@@ -2,7 +2,24 @@
 {
     public class Task
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Description { get; set; }
+        public Status ActualStatus { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public enum Status
+        {
+            Todo,
+            InProgress,
+            Done
+        }
+
+        public Task()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+        }
     }
 }
